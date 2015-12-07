213aaff790c9bdb9f8625a521907042f588b39a1
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-12-07 12:52:40 +0100

    Added a new testplugin2 whith the blendNode. Change both CMakeLists.txt to compile the helloworld

---
+++ TestPluginMaya/build2/src/CMakeFiles/testPluginMaya2.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Emma/Documents/maya/projects/default/scenes/Maya-Soft-Body-Simulation/TestPluginMaya/src/BlendNode.cpp" "/Users/Emma/Documents/maya/projects/default/scenes/Maya-Soft-Body-Simulation/TestPluginMaya/build2/src/CMakeFiles/testPluginMaya2.dir/BlendNode.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "OSMac_"
+  "REQUIRE_IOSTREAM"
+  "_BOOL"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Applications/Autodesk/maya2016/devkit/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
