6099c4d2f55f0d37421944a00a7043509ff04144
ellenhager <ellenhager@c-112-132.eduroam.liu.se>
2015-12-09 16:30:29 +0100

    Replaced 'Test'-plugin with custom nCloth nucleus solver.

--- SoftBodySimulationPlugin/include/Test.h
+++ SoftBodySimulationPlugin/include/Test.h
@@ -1,4 +1,4 @@
-#ifndef _TESTNUCLEUSNODE_
+/*#ifndef _TESTNUCLEUSNODE_
 #define _TESTNUCLEUSNODE_
 
 //-
@@ -54,6 +54,41 @@ public:
     static MObject nextState;
     static MObject currentTime;
 
+};
+ 
+#endif*/
+
+#ifndef _TESTNPASSIVENODE_
+#define _TESTNPASSIVENODE_
+//-
+// ==========================================================================
+// Copyright 1995,2006,2008 Autodesk, Inc. All rights reserved.
+//
+// Use of this software is subject to the terms of the Autodesk
+// license agreement provided at the time of installation or download,
+// or which otherwise accompanies this software in either electronic
+// or hard copy form.
+// ==========================================================================
+//+
+#include <maya/MPxNode.h>
+#include <maya/MnRigid.h>
+class testNpassiveNode : public MPxNode
+{
+public:
+    virtual MStatus compute(const MPlug &plug, MDataBlock &dataBlock);
+    testNpassiveNode();
+    static void *creator() 
+    { 
+        return new testNpassiveNode; 
+    }
+    static MStatus initialize();
+    static const MTypeId id;
+    // attributes
+    static MObject currentState;
+    static MObject startState;
+    static MObject currentTime;
+    static MObject inputGeom;
+    MnRigid  fNObject;
 };
  
 #endif
