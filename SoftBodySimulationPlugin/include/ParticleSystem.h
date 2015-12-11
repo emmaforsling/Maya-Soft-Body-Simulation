7214d062e99c70e809dd364993e2cc5f85b8b508
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-12-11 12:22:58 +0100

    Added the functions updatePositions and updateVelocities in ParticleSystem

--- SoftBodySimulationPlugin/include/ParticleSystem.h
+++ SoftBodySimulationPlugin/include/ParticleSystem.h
@@ -3,6 +3,7 @@
 
 #include <maya/MGlobal.h>
 #include <maya/MStatus.h>
+#include <maya/MFloatVectorArray.h>
 
 class ParticleSystem
 {
@@ -10,8 +11,19 @@ public:
 	ParticleSystem();
 	~ParticleSystem();
 
-private:
+	void updateForces(float dt);		//
+	void updateVelocities(float dt);	//Euler, call first
+	void updatePositions(float dt);		//Euler, call after updateVelocities()
+	
 
+	// a = F/m
+	// v = a * dt
+	// p = v * dt
+
+private:
+	MFloatVectorArray F;	// Force
+	MFloatVectorArray v; 	// Velocity
+	MFloatVectorArray p;	// Position
 };
 
 #endif
